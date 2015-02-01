124487f863fd2180f15f3764831e4e86204b8659
Hideaki Suzuki <h2suzuki@gmail.com>
2015-02-01 14:42:48 +0900

    Import project files

---
+++ TimeElapsed.hpp
@@ -0,0 +1,77 @@
+#ifndef __TIME_ELAPSED_HPP__
+#define	__TIME_ELAPSED_HPP__
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#ifdef NO_TIME_ELAPSED_MEASUREMENT
+#  define	TIME_ELAPSED_N(n,body)					time_elapsed_impl_noop<n>([&]{body;})
+#  define	TIME_ELAPSED_EXP_N(n,body)				time_elapsed_impl_noop<n>([&]{body;})
+#  define	TIME_ELAPSED_MARKER_N(n,marker,body)	time_elapsed_impl_noop<n>([&]{body;})
+#else
+#  define	TIME_ELAPSED_N(n,body)					time_elapsed_impl1<n>([&]{body;},__FILE__,__LINE__)
+#  define	TIME_ELAPSED_EXP_N(n,body)				time_elapsed_impl2<n>([&]{body;},#body)
+#  define	TIME_ELAPSED_MARKER_N(n,marker,body)	time_elapsed_impl_core<n>([&]{body;},marker)
+#endif
+
+#define	TIME_ELAPSED(body)							TIME_ELAPSED_N(1,body)
+#define	TIME_ELAPSED_EXP(body)						TIME_ELAPSED_EXP_N(1,body)
+#define	TIME_ELAPSED_MARKER(marker,body)			TIME_ELAPSED_MARKER_N(1,marker,body)
+
+#define	TIME_ELAPSED_HEADER							"ElapsedTime: "
+#define	TIME_ELAPSED_HEADER_WIDTH					48
+
+
+template <const int count, typename BlockBody>
+inline void time_elapsed_impl_core(BlockBody body, const std::string& header, std::ostream &o = std::cout)
+{
+	using namespace std;
+	using namespace std::chrono;
+
+	auto t0 = high_resolution_clock::now();
+
+	for (int i = 0; i < count; i++)
+		body();
+
+	auto t1 = high_resolution_clock::now();
+
+
+	// Now, print out the elapsed time with its header
+	auto prev_flags = o.flags();
+	auto prev_precision = o.precision();
+	auto prev_width = o.width();
+
+	o << left << setw(TIME_ELAPSED_HEADER_WIDTH) << (TIME_ELAPSED_HEADER + header);
+	o << "cnt:" << setw(8) << count;
+	o << setw(12) << right << fixed << setprecision(3)
+		<< (float)duration_cast<microseconds>(t1 - t0).count()/1000 << " msec" << endl;
+
+	o.width(prev_width);
+	o.precision(prev_precision);
+	o.flags(prev_flags);
+}
+
+template <const int count, typename BlockBody>
+inline void time_elapsed_impl_noop(BlockBody body, std::ostream &o = std::cout)
+{
+	for (int i = 0; i < count; i++)
+		body();
+}
+
+template <int count, typename BlockBody>
+inline void time_elapsed_impl1(BlockBody body, const char * const path, const int lno, std::ostream &o = std::cout)
+{
+	std::string basename{ path + std::string(path).find_last_of("\\/") + 1 };
+	time_elapsed_impl_core<count>(body, basename + " at L." + std::to_string(lno), o);
+}
+
+template <int count, typename BlockBody>
+inline void time_elapsed_impl2(BlockBody body, const char * const bodystr, std::ostream &o = std::cout)
+{
+	std::string heading{ std::string(bodystr).substr(0,TIME_ELAPSED_HEADER_WIDTH - sizeof(TIME_ELAPSED_HEADER) - 2)	};
+	time_elapsed_impl_core<count>(body, heading, o);
+}
+
+#endif // !__TIME_ELAPSED_HPP__
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <thread>	// for sleep_for()
+#include <chrono>	// for second()
+#include <cmath>	// for sqrt()
+
+#include <iostream>
+
+//#define NO_TIME_ELAPSED_MEASUREMENT
+#include "TimeElapsed.hpp"
+
+void slow_function(int t = 2)
+{
+	std::this_thread::sleep_for(std::chrono::seconds(t));
+}
+
+int main(void)
+{
+	float f;
+
+	// 1. Easy to use
+	TIME_ELAPSED(slow_function());
+
+	// 2. Can refer to the caller environment by lambda [&]
+	f = 1.0f;
+	TIME_ELAPSED(f += sqrtf(f));
+	std::cout << "f = " << f << std::endl;
+
+	// 3. With the number of cycles: 500000 times
+	f = 1.0f;
+	TIME_ELAPSED_N(500000, f += sqrtf(f));
+	std::cout << "f = " << f << std::endl;
+
+
+	// 4. Can pass a code block: n can live only within (...)
+	f = 1.0f;
+	TIME_ELAPSED(
+
+		int n = 3;
+		for (int i = 0; i < n; i++) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			f++;
+		}
+
+	);
+	std::cout << "f = " << f << std::endl;
+
+	// 5. With repetitions: this code block runs three times
+	TIME_ELAPSED_N(3,
+
+		int m = 2;
+		for (int i = 0; i < m; i++) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			f++;
+		}
+
+	);
+	std::cout << "f = " << f << std::endl;
+
+	// 6. A distinct line can be the line header by itself
+	TIME_ELAPSED_EXP(f = cosh(f));
+
+	// 7. Your custom header
+	TIME_ELAPSED_MARKER("My Header 1",
+
+		for (int i = 0; i < 2; i++) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+
+	);
+
+	// 7. Your custom header with repetition
+	TIME_ELAPSED_MARKER_N(2, "My Location 2",
+
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+
+	);
+
+	return 0;
+}
